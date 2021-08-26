08b777e82f195ed97534f069f35f1f1dada7a976
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-08-25 22:07:20 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro/Services/ProductViewModelService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Cadastro.Domain.Entities;
+using Cadastro.Domain.Interfaces;
+
+using Cadastro.Interfaces;
+using Cadastro.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.Services
+{
+    public class ProductViewModelService : IProductViewModelService
+    {
+        private readonly IProductRepository _clientRepository;
+        private readonly IMapper _mapper;
+
+        public ProductViewModelService(IProductRepository clientRepository, IMapper mapper)
+        {
+            _clientRepository = clientRepository;
+            _mapper = mapper;
+        }
+
+        public void Delete(int id)
+        {
+            _clientRepository.Delete(id);
+        }
+
+        public ProductViewModel Get(int id)
+        {
+            var entity = _clientRepository.Get(id);
+            if (entity == null)
+                return null;
+
+            return _mapper.Map<ProductViewModel>(entity);
+        }
+
+        public IEnumerable<ProductViewModel> GetAll()
+        {
+            var list = _clientRepository.GetAll();
+            if (list == null)
+                return new ProductViewModel[] { };
+
+            return _mapper.Map<IEnumerable<ProductViewModel>>(list);
+        }
+
+        public void Insert(ProductViewModel viewModel)
+        {
+            var entity = _mapper.Map<Product>(viewModel);
+
+            _clientRepository.Insert(entity);
+        }
+
+        public void Update(ProductViewModel viewModel)
+        {
+            var entity = _mapper.Map<Product>(viewModel);
+
+            _clientRepository.Update(entity);
+        }
+    }
+}
---
+++ Cadastro/ViewModels/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "O código é requerido.")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome é requerido.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Value")]
+        [Required(ErrorMessage = "O valor do produto é requerido")]
+        public decimal Value { get; set; }
+
+        [Required(ErrorMessage = "O email é requerido.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Ativo")]
+        public bool Ative { get; set; }
+    }
+}
